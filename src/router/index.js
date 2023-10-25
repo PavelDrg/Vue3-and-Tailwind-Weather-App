964cda8731c4d91d9abae08dec6091598ae00f49
PavelDrg <pavel.dragan2@gmail.com>
2023-10-25 23:50:27 +0300

    Added transition between pages andchanged tab names

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,28 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: "Home",
+      },
     },
     {
       path: "/weather/:country/:city",
       name: "cityView",
       component: CityView,
+      meta: {
+        title: "Weather",
+      },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.country
+      ? `${to.params.city}, ${to.params.country}`
+      : to.meta.title
+  } | The Local Weather`;
+  next();
 });
 
 export default router;
